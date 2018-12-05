4e6c25ed2fe4effcc9590038e4e24a5fff2598ba
Juan Guzmán <jdguzmans@hotmail.com>
2018-12-05 12:50:51 -0500

    edited config and released first version of doc

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
   STORE_PASS: process.env.STORE_PASS,
   KEY_PASS: process.env.KEY_PASS,
   ALIAS_NAME: process.env.ALIAS_NAME,
-  PACKAGE_NAME: process.env.PACKAGE_NAME
+  PACKAGE_NAME: 'com.evancharlton.mileage',
+  PARALLEL_EXECUTIONS_AT_SAME_TIME: 2
 }
